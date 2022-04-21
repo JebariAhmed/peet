0d35091b32e53002b3c9e1e0905dc24bbe86ab58
JebariAhmed <bouhmidjetbrain@gmail.com>
2022-04-20 13:39:40 -1100

    hello

--- back/models/User.js
+++ back/models/User.js
@@ -1,5 +1,7 @@
-const mongoose= require("mongoose");
-const UserSchema = new mongoose.Schema(
+
+//lena bch nasnaa l base donne 
+const mongoose= require("mongoose");// aayt ll monous
+const UserSchema = new mongoose.Schema(// bch naaml tableaux fl base taai "schema "
     {
         username:{type:String,required:true,unique:true},
         email:{type:String,required:true,unique:true},
